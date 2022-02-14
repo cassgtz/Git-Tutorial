2c20e46f8a525672222b16fba775a49ddbd785c4
cassgtz <98518446+cassgtz@users.noreply.github.com>
2022-02-13 17:52:19 -0800

    Update demo.c

--- demo.c
+++ demo.c
@@ -1,22 +1,11 @@
 # include <stdio.h>
 
-// a function that takes an unsigned integer and returns the Hamming Weight (number of '1' bits it has)
-int hamWeight(char biString[32]){
-	int num = 0; 
-	for (int i =0; i < 32; ++i){
-		if (biString[i] == 1){
-			++num;
-		}
-	}
-	return num;
-}
-
 int main(void) {
-	char ex1[32] = '00000000000000000000000000001011';
-	printf("%d\n", hamWeight(ex1)); // prints 3
+	int piday = 314;
+	printf("%d\n", piday); 
 
-	char ex2[32] = '00000000000000000000000010000000';
-	printf("%d\n", hamWeight(ex2)); // prints 1
+	int vday = 214;
+	printf("%d\n", vday);
 
 	return 0;
 }
